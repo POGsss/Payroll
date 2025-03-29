88013824d87ffe991e8469cf2cc6ccb6eb59b4c1
unknown <frankiepinat14@gmail.com>
2025-03-29 16:28:19 +0800

    Upload Files

---
+++ app/src/main/java/com/example/payroll/MainAdapter.java
@@ -0,0 +1,41 @@
+package com.example.payroll;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainAdapter extends FirebaseRecyclerAdapter<MainModel, MainAdapter.myViewHolder> {
+    public MainAdapter(@NonNull FirebaseRecyclerOptions<MainModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull MainAdapter.myViewHolder holder, int position, @NonNull MainModel model) {
+
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.employee_item, parent, false);
+        return null;
+    }
+
+    public class myViewHolder extends RecyclerView.ViewHolder {
+        // Declarations
+        String name, employeeType;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+        }
+    }
+}
