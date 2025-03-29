6b5b436ec81a08fbe8ae7ccd97e14b5ec01bbda6
zhyyy-idk <ompocralphzhyros@gmail.com>
2025-03-29 14:53:43 +0800

    Initialized Views

--- app/src/main/java/com/example/payroll/AddActivity.java
+++ app/src/main/java/com/example/payroll/AddActivity.java
@@ -3,6 +3,8 @@ package com.example.payroll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        EditText etID = findViewById(R.id.etID);
+        EditText etName = findViewById(R.id.etName);
+        Spinner spinnerEmpType = findViewById(R.id.spinnerEmpType);
+        EditText etWorkHours = findViewById(R.id.etWorkhours);
+        EditText etWorkDays = findViewById(R.id.etWorkdays);
 
     }
 }
